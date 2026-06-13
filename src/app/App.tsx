4622ff4c56d98e8b2895e57bb011a94d5cbfade4
蒙煋Evanflare <evanflare@outlook.com>
2026-06-13 11:31:27 +0800

    ui调整

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -84,8 +84,8 @@ export default function App() {
     <div className="size-full flex flex-col min-h-screen bg-background text-foreground ">
       {/* Header (Android 时显示暗黑切换和 Log) */}
       {isAndroid && (
-        <header className="w-full border-b border-sidebar-border bg-sidebar flex-one px-4 py-3 flex items-center justify-between">
-          <div className="flex items-center gap-3 py-3">
+        <header className="w-full border-b border-sidebar-border bg-sidebar flex-one px-4 pt-6 pb-2 flex items-center justify-between">
+          <div className="flex items-center gap-3">
             <div className="w-9 h-9 rounded-xl bg-primary flex items-center justify-center shrink-0">
               <ShieldCheck size={20} className="text-primary-foreground" />
             </div>
